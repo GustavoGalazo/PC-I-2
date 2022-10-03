680adb59834c01b6cbb8ff6dfa286923d363dfd0
Gustavo.Galazo <112669959+GustavoGalazo@users.noreply.github.com>
2022-10-03 19:47:42 -0300

    atualizado

--- README.m
+++ README.m
@@ -10,22 +10,19 @@ Declaração:
 Numero1: (nteiro); 
 
   
+Numero2: (inteiro); 
 
-Numero2: (nteiro); 
 
+Numero3: (intero);
   
 
 Inicio 
+  
 
-   
-
-  Escreva  ("Digite numero 1"); 
-
-          
+Escreva  ("Digite numero 1"); 
+         
 
 Leia(numero1; 
-
-  
 
   
 
@@ -39,18 +36,24 @@ Leia(numero2;
 
 SE (numero1>numero2) ENTAO 
 
-  
-
-         Escreva (" O Maior numero1  maior que numero2) 
+   Escreva (" O Maior numero1  maior que numero2 ") 
+         
 
 SENAO 
 
      SE (numero2>numero1) 
 
-      
+ fim-se
+ 
+SE (numero3 > numero2 ) 
+     
+     Escreva (" O Maior numero2 maior numero1 ") 
+     
+ SENAO 
 
-     Escreva (" O Maior numero2 maoir que numero1) 
+    Escreva  ( " O nuemro 3 maior que numero 2 " ) 
+    
 
-fimse 
-
+fim-se 
+     
 fim-algarotimo 
