6d3c29a972ee8319778bd65f35cb372753b418b3
Gustavo.Galazo <112669959+GustavoGalazo@users.noreply.github.com>
2022-10-03 19:20:21 -0300

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,56 @@
+# PC-I-2
+Algoritimo "Maior Menor" 
+
+  
+
+Declaração: 
+
+  
+
+Numero1: (nteiro); 
+
+  
+
+Numero2: (nteiro); 
+
+  
+
+Inicio 
+
+   
+
+  Escreva  ("Digite numero 1"); 
+
+          
+
+Leia(numero1; 
+
+  
+
+  
+
+Escreva ("Digite numero2"); 
+
+      
+
+Leia(numero2; 
+
+  
+
+SE (numero1>numero2) ENTAO 
+
+  
+
+         Escreva (" O Maior numero1  maior que numero2) 
+
+SENAO 
+
+     SE (numero2>numero1) 
+
+      
+
+     Escreva (" O Maior numero2 maoir que numero1) 
+
+fimse 
+
+fim-algarotimo 
